a92e53c8c6228ca978c7a77324d749eb9f2c12e9
brandongasser <bgasser12@gmail.com>
2023-11-25 21:51:33 -0600

    Added Stack and Queue to utilities

---
+++ utilities/queue.hs
@@ -0,0 +1,45 @@
+module Utilities.Queue where
+
+data Queue a = Queue {
+    inbox :: [a],
+    outbox :: [a]
+} deriving (Foldable)
+
+instance (Show a) => Show (Queue a) where
+    show queue = "Queue " ++ show (inbox queue ++ outbox queue)
+
+instance Functor Queue where
+    fmap f queue = Queue { inbox = map f $ inbox queue, outbox = map f $ outbox queue }
+
+instance Applicative Queue where
+    pure x = Queue { inbox = [], outbox = pure x }
+    q1 <*> q2 = Queue { inbox = [], outbox = toList q1 <*> toList q2 }
+
+instance Monad Queue where
+    queue >>= f = Queue { inbox = inbox queue >>= reverse . toList . f, outbox = outbox queue >>= toList . f }
+
+empty :: Queue a
+empty = fromList []
+
+fromList :: [a] -> Queue a
+fromList = Queue []
+
+toList :: Queue a -> [a]
+toList queue = inbox queue ++ reverse (outbox queue)
+
+enqueue :: a -> Queue a -> Queue a
+enqueue x queue = Queue { inbox = x : inbox queue, outbox = outbox queue }
+
+enqueueAll :: [a] -> Queue a -> Queue a
+enqueueAll xs queue = foldl (flip enqueue) queue xs
+
+dequeue :: Queue a -> (Maybe a, Queue a)
+dequeue queue | null (outbox queue) && null (inbox queue) = (Nothing, queue)
+              | null (outbox queue) = dequeue $ Queue { inbox = [], outbox = reverse $ inbox queue }
+              | otherwise = (Just $ head $ outbox queue, Queue { inbox = inbox queue, outbox = tail $ outbox queue })
+
+dequeueMany :: Int -> Queue a -> ([Maybe a], Queue a)
+dequeueMany n queue | n <= 0 =    ([], queue)
+                    | otherwise = let (x, queue') = dequeue queue
+                                      (xs, queue'') = dequeueMany (n - 1) queue'
+                                  in  (x:xs, queue'')
---
+++ utilities/stack.hs
@@ -0,0 +1,39 @@
+module Utilities.Stack ( Stack, fromList, push, pop ) where
+
+newtype Stack a = Stack [a]
+    deriving (Read, Show, Foldable)
+
+instance Functor Stack where
+    fmap f (Stack xs) = Stack (map f xs)
+
+instance Applicative Stack where
+    pure x = Stack $ pure x
+    (Stack xs) <*> Stack ys = Stack (xs <*> ys)
+
+instance Monad Stack where
+    (Stack xs) >>= f = foldr ((\(Stack ys) (Stack acc) -> Stack (ys ++ acc)) . f) (Stack []) xs
+
+empty :: Stack a
+empty = fromList []
+
+fromList :: [a] -> Stack a
+fromList = Stack
+
+toList :: Stack a -> [a]
+toList (Stack xs) = xs
+
+push :: a -> Stack a -> Stack a
+push x (Stack xs) = Stack (x:xs)
+
+pushAll :: [a] -> Stack a -> Stack a
+pushAll xs stack = foldl (flip push) stack xs
+
+pop :: Stack a -> (Maybe a, Stack a)
+pop (Stack []) = (Nothing, Stack [])
+pop (Stack (x:xs)) = (Just x, Stack xs)
+
+popMany :: Int -> Stack a -> ([Maybe a], Stack a)
+popMany n stack | n <= 0 =    ([], stack)
+                | otherwise = let (x, stack')   = pop stack
+                                  (xs, stack'') = popMany (n - 1) stack'
+                              in  (x:xs, stack'')
