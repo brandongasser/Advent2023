80edd6b27bf6ce8ea1056fe8644870d5809a60dc
brandongasser <bgasser12@gmail.com>
2023-12-02 13:05:54 -0600

    Day 2 Part 2 Solved

--- day2/part2.hs
+++ day2/part2.hs
@@ -1,4 +1,13 @@
 module Day2.Part2 ( main ) where
 
+import Day2.Parser ( Game(..), Draw(Draw), Color(Blue, Red, Green), parseFile )
+
 main :: IO ()
-main = undefined
+main = do games <- parseFile "day2/input.txt"
+          print $ sum $ map gamePower games
+
+gamePower :: Game -> Int
+gamePower (Game _ fds) = colorMax Red * colorMax Green * colorMax Blue
+    where
+        flatFds = concat fds
+        colorMax color = maximum $ map (\(Draw _ n) -> n) $ filter (\(Draw c _) -> c == color) flatFds
