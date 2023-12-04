0c32c7fada79fe85d55a1b47139c4d8ba4d15ea0
brandongasser <bgasser12@gmail.com>
2023-12-04 09:56:58 -0600

    Updated variable name

--- day4/part2.hs
+++ day4/part2.hs
@@ -7,8 +7,8 @@ handleCards :: Card -> State [Int] ()
 handleCards originalCard = do currentCounts <- get
                               let winCount = wins originalCard
                               let winCards = [cardId originalCard + 1..cardId originalCard + winCount]
-                              let addVector = take (length currentCounts) [if i `elem` winCards then currentCounts !! (cardId originalCard - 1) else 0 | i <- [1..]]
-                              put $ addVectors currentCounts addVector
+                              let newCards = take (length currentCounts) [if i `elem` winCards then currentCounts !! (cardId originalCard - 1) else 0 | i <- [1..]]
+                              put $ addVectors currentCounts newCards
 
 addVectors :: [Int] -> [Int] -> [Int]
 addVectors = zipWith (+)
